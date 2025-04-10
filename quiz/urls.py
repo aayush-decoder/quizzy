731757e768d73fa276cbe9722044c4569441f2f1
Aayush Prasad <aayushp336@gmail.com>
2025-04-10 21:06:26 +0530

    defined urls and created template dir

--- quiz/urls.py
+++ quiz/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.login_page, name="login_page"),
+    path('register/', views.register_page, name="register_page"),
 ]
---
+++ quiz/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def login_page(request):
+    return render(request, 'login.html')
+
+def register_page(request):
+    return render(request, 'signup.html')
