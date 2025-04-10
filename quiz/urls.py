2638dfb42c9cca3088ab73f797f3adba6fcab11f
Aayush Prasad <aayushp336@gmail.com>
2025-04-10 21:24:46 +0530

    linked css and handled media in templates

--- quiz/urls.py
+++ quiz/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -22,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.login_page, name="login_page"),
     path('register/', views.register_page, name="register_page"),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
