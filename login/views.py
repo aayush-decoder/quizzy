e370b95a9b1494214a190ea26c30da6ab85ccceb
sharvya2803 <shravyapatel2806@gmail.com>
2025-04-16 20:26:13 +0530

    authentication done

--- login/views.py
+++ login/views.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 from django.contrib.auth import authenticate, login
 from django.shortcuts import redirect
 from django.conf import settings
+from django.contrib.auth.models import User
 
 # Create your views here.
 def login_view(request):
@@ -18,13 +19,34 @@ def login_view(request):
             return redirect('home')
         else:
             error_message = "Invalid username or password"
-    return render(request, 'login/login.html')
+            return render(request, 'home.html', {'error_message': error_message})
+    return render(request, 'login.html')
 
 def signup_view(request):
-    return render(request, 'login/signup.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        conpass = request.POST.get('confirm_password')
+        email = request.POST.get('email')
+        # Here you would typically create a new user
+        # For example:
+        # user = User.objects.create_user(username=username, password=password, email=email)
+        # user.save()
+        # return redirect('login')
+        user = User.objects.create_user(username=username, email=email)
+        user.set_password(password)  # Set the password using set_password method
+        if password == conpass:
+            user.save()
+            return redirect('login')
+        else:
+            error_message = "Passwords do not match"
+            return render(request, 'signup.html', {'error_message': error_message})
+        
+       
+    return render(request, 'signup.html')
 
 def forgetpass(request):
-    return render(request, 'login/forgetpass.html')
+    return render(request, 'forgetpass.html')
 
 def home_view(request):
-    return render(request, 'home.html')
+    return render(request,'home.html')
--- quiz/urls.py
+++ quiz/urls.py
@@ -21,8 +21,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.admin, name="home"),
-    path('admin/', admin.site.urls),
+    path('', include('login.urls')),
     path('auth/', include('login.urls')),
     path('quiz/', views.quiz, name="quiz"),
 
