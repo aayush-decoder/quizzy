32b1175e94e75dfb6aa6cfa2bebc94f4f6827fbe
Aayush Prasad <aayushp336@gmail.com>
2025-04-24 00:56:20 +0530

    completed quiz working

--- login/urls.py
+++ login/urls.py
@@ -6,9 +6,9 @@ from . import views
 
 urlpatterns = [
    path('', views.home_view, name='home'),
-   path('login/', views.login_view, name='login'),
+   path('auth/login/', views.login_view, name='login'),
    # path('login2/', views.login_view, name='login'),
-   path('signup/', views.signup_view, name='signup'),
+   path('auth/signup/', views.signup_view, name='signup'),
    path('forgetpass/', views.forgetpass, name='forgetpass'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
